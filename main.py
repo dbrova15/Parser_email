6532658c2e7b3e1cc4fec4d2b8630b3408598844
Dimon151 <d.brova15@gmail.com>
2017-12-16 15:57:41 +0200

    finish

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def parse_link(link):  # парсер ссылок
     soup = BeautifulSoup(html, "lxml")
     link_list = []
     for row in soup.find_all('a', attrs={'href': re.compile("^http://")}):
-        # print(row)
         link_list.append(str(row.get('href')))
     return link_list
 
@@ -51,38 +50,48 @@ def parse_mail(html):  # парсер email
 
 
 if __name__ == '__main__':
-    url = str(input("Сайт для парсинга: "))
+    url = str(input("Сайт для парсинга: "))  #.replace("http://", "")
 
+    urls = []
+    urls_old = []
     mail_list = []
     url_new = []
 
     try:
-        urls = parse_link(url)
-        if urls is None:
-            exit()
-    except requests.exceptions.MissingSchema:
-        print("Ссылка '{}' работает. Проверьте правильность ввода и повторите ввод: ".format(url))
-        url = str(input("\nСайт для парсинга: "))
-        try:
-            urls = parse_link(url)
-        except requests.exceptions.MissingSchema:
-            print("Ссылка не работает. Завершаем работу")
-            exit()
-
-    urls_old = urls.copy()
-    urls = [i for i, _ in groupby(urls)]  # удаляем дубли
-    try:
         n = int(input("Глубина парсинга: "))
     except ValueError:
         print("Вы ввели НЕ число, посторите ввод.")
-        n = int(input("Глубина парсинга: "))
+        n = int(input("Глубина парсинга, от 1 до -1 (больше 1 будет работать рекурсивно): "))
 
+    """Если ввели не правильную цифру"""
     if n < 0:
         n = n * -1
     elif n == 0:
         n = 1
 
-    for l in range(1, n + 1):
+    """Далее определение поведения парсера работать рекурсивно или парсить данные только с одной ссылки"""
+    if n != 1:
+        try:
+            urls = parse_link(url)
+            if urls is None:
+                exit()
+        except requests.exceptions.MissingSchema:
+            print("Ссылка '{}' работает. Проверьте правильность ввода и повторите ввод: ".format(url))
+            url = str(input("\nСайт для парсинга: "))
+            try:
+                urls = parse_link(url)
+            except requests.exceptions.MissingSchema:
+                print("Ссылка не работает. Завершаем работу")
+                exit()
+
+        urls_old = urls.copy()
+        urls = [i for i, _ in groupby(urls)]  # удаляем дубли
+    else:
+        urls.append(url)
+        urls_old = urls.copy()
+
+    """ Начинаем работу """
+    for l in range(0, n):
         time_start = time.time()
         print("Уровень парсинга {}".format(l))
         len_start = len(urls)
@@ -90,11 +99,13 @@ if __name__ == '__main__':
             link = urls.pop()
             html = requests.get(str(link), headers=headers).text  # получаем html-код страницы
             mail_list.extend(parse_mail(html))  # парсим електронные почты
-            urls_new = parse_link(link)  # парсим новые урлы
-            if urls_new is not None:
-                url_new.extend(urls_new)
-            print("Проверено {}% ссылок.".format(round((len(urls) / len_start * 100), 2)))
-        print("урлов после парсинга ", urls)
+            if n != 1:
+                urls_new = parse_link(link)  # парсим новые урлы
+                if urls_new is not None:
+                    url_new.extend(urls_new)
+                print("Проверено {}% ссылок.".format(round((len(urls) / len_start * 100), 2)))
+            if n == 1:
+                break
 
         for i in urls_old:  # чистим список спаршеных урлов от тех которые уже были в работе скрипта
             if i in url_new:
@@ -104,7 +115,7 @@ if __name__ == '__main__':
         urls.extend(url_new)  # добавлям новые урлы для парсинга
         urls = [i for i, _ in groupby(urls)]  # удаляем дубли
         url_new.clear()
-        t = round((time_start - time.time() / 60), 2)
+        t = round(((time.time() - time_start) / 60), 2)
         print("=" * 82)
         print("\nПрошли уровень парсинга {} за {} минуты. Спаршно {} електронных адресов.\n".format(l, t, len(mail_list)))
         print("=" * 82)
